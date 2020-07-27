d1b0bdd65fc64fe826c3fa85d61032d031d921a0
lolyparty <ololadeolayiwola01@gmail.com>
2020-07-27 22:55:13 +0100

    scrolltop

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-
-
-   var closenav = function(){
+  var closenav = function(){
     document.querySelector('.slider').classList.add('closure');
     document.querySelector('.slider').classList.remove('slide-nav');
 }
@@ -21,3 +19,24 @@ for(var i = 0; i < list.length; i++){
 
 document.querySelector('.closebtn').addEventListener('click',closenav);
 
+let scrollHeight = Math.max(document.documentElement.offsetHeight,document.documentElement.scrollHeight, document.documentElement.clientHeight, document.body.scrollHeight, document.body.offsetHeight, document.body.clientHeight);
+// console.log(scrollHeight)
+
+setInterval(()=>{
+  
+var scrollPos = window.pageYOffset;
+// console.log(scrollPos) 
+
+if(scrollPos > 2800){
+  document.querySelector('.scroll_top').style.display = 'block';
+}else if(scrollPos > 4200){
+    document.querySelector('.fa').style.color = 'white !important'
+}
+else{
+  document.querySelector('.scroll_top').style.display = 'none'
+}
+}, 100)
+
+document.querySelector('.scroll_top').addEventListener('click', ()=>{
+  window.scrollTo(0,0)
+})
