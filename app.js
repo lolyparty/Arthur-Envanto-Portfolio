d16ef7b9247463f789ee289ddacfe3ad4ff13e65
lolyparty <ololadeolayiwola01@gmail.com>
2022-04-20 03:31:14 +0100

    updated local repo

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-  var closenav = function(){
-    document.querySelector('.slider').classList.add('closure');
-    document.querySelector('.slider').classList.remove('slide-nav');
+var closenav = function(){
+  document.querySelector('.slider').classList.add('closure');
+  document.querySelector('.slider').classList.remove('slide-nav');
 }
 
 var opennav = function(){
-    document.querySelector('.slider').classList.add('slide-nav');
-    document.querySelector('.slider').classList.remove('closure'); 
+  document.querySelector('.slider').classList.add('slide-nav');
+  document.querySelector('.slider').classList.remove('closure'); 
 }   
 
 //nav bar
@@ -14,7 +14,7 @@ document.querySelector('.navbar-toggler').addEventListener('click',opennav);
 var list = document.getElementsByClassName('navlink');
 console.log(list)
 for(var i = 0; i < list.length; i++){
-    list[i].addEventListener('click',closenav);
+  list[i].addEventListener('click',closenav);
 }
 
 document.querySelector('.closebtn').addEventListener('click',closenav);
@@ -23,20 +23,20 @@ let scrollHeight = Math.max(document.documentElement.offsetHeight,document.docum
 // console.log(scrollHeight)
 
 setInterval(()=>{
-  
+
 var scrollPos = window.pageYOffset;
 // console.log(scrollPos) 
 
 if(scrollPos > 2800){
-  document.querySelector('.scroll_top').style.display = 'block';
+document.querySelector('.scroll_top').style.display = 'block';
 }else if(scrollPos > 4200){
-    document.querySelector('.fa').style.color = 'white !important'
+  document.querySelector('.fa').style.color = 'white !important'
 }
 else{
-  document.querySelector('.scroll_top').style.display = 'none'
+document.querySelector('.scroll_top').style.display = 'none'
 }
 }, 100)
 
 document.querySelector('.scroll_top').addEventListener('click', ()=>{
-  window.scrollTo(0,0)
+window.scrollTo(0,0)
 })
